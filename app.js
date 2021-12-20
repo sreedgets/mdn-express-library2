97f364cb4ec99a5a54518d3a6e37c14d0d4257ea
Skyler <sreedgets@outlook.com>
2021-12-20 16:31:00 -0600

    Added routes and route handler functions

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error'));
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const catalogRouter = require('./routes/catalog');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
